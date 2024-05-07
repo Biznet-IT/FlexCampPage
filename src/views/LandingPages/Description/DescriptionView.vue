9f9606a26438f75db1af7b91440919fb94b5eeba
Sebastián Gómez Letelier <53100726+TheGreySam@users.noreply.github.com>
2024-05-07 17:12:47 -0400

    Update DescriptionView.vue

--- src/views/LandingPages/Description/DescriptionView.vue
+++ src/views/LandingPages/Description/DescriptionView.vue
@@ -4,9 +4,10 @@ import { onMounted, onUnmounted } from "vue";
 //example components
 import DefaultNavbar from "../../../examples/navbars/NavbarDefault.vue";
 import DefaultFooter from "../../../examples/footers/FooterDefault.vue";
+import DefaultInfoCard from "../../../examples/cards/infoCards/DefaultInfoCard.vue";
 
 //image
-import bg0 from "@/assets/img/bg9.jpg";
+import bg0 from "@/assets/img/containers/container2.png";
 
 //dep
 import Typed from "typed.js";
@@ -42,63 +43,52 @@ onUnmounted(() => {
 });
 </script>
 <template>
-  <DefaultNavbar
-    :action="{
-      route: 'javascript:;',
-      label: 'Buy Now',
-      color: 'btn-white',
-    }"
-    transparent
-  />
+  <DefaultNavbar :action="{
+    route: 'contactus',
+    label: 'Contacto',
+    color: 'btn-white',
+  }" transparent />
   <header class="bg-gradient-dark">
-    <div
-      class="page-header min-vh-75"
-      :style="{ backgroundImage: `url(${bg0})` }"
-    >
+    <div class="page-header min-vh-50" :style="{ backgroundImage: `url(${bg0})` }">
       <span class="mask bg-gradient-dark opacity-6"></span>
       <div class="container">
         <div class="row justify-content-center">
           <div class="col-lg-8 text-center mx-auto my-auto">
             <h1 class="text-white">
-              Work with an amazing <span class="text-white" id="typed"></span>
+              Descripción del Servicio
             </h1>
-            <div id="typed-strings">
-              <h1>team</h1>
-              <h1>design</h1>
-              <h1>tool</h1>
-            </div>
             <p class="lead mb-4 text-white opacity-8">
-              We’re constantly trying to express ourselves and actualize our
-              dreams. If you have the opportunity to play this game
+              
             </p>
-            <button type="submit" class="btn bg-white text-dark">
-              Create Account
-            </button>
-            <h6 class="text-white mb-2 mt-5">Find us on</h6>
-            <div class="d-flex justify-content-center">
-              <a href="javascript:;"
-                ><i class="fab fa-facebook text-lg text-white me-4"></i
-              ></a>
-              <a href="javascript:;"
-                ><i class="fab fa-instagram text-lg text-white me-4"></i
-              ></a>
-              <a href="javascript:;"
-                ><i class="fab fa-twitter text-lg text-white me-4"></i
-              ></a>
-              <a href="javascript:;"
-                ><i class="fab fa-google-plus text-lg text-white"></i
-              ></a>
-            </div>
           </div>
         </div>
       </div>
     </div>
   </header>
-  <!-- <div class="card card-body shadow-xl mx-3 mx-md-4 mt-n6">
-    <Information />
-    <AboutTeam />
-    <Featuring />
-    <Newsletter />
-  </div> -->
+  <div class="container">
+    <div class="container p-5">
+      <div class="row justify-content-center">
+        <DefaultInfoCard icon="content_copy" title="Arriendo" description="FlexCamp ofrece una innovadora propuesta de arriendo de campamentos que no requiere permisos de
+              construcción, facilitando así una instalación y despliegue extremadamente rápidos.
+              Esta característica es crucial para proyectos que necesitan ponerse en marcha de manera inmediata,
+              eliminando las demoras asociadas a la gestión de autorizaciones y permisos." />
+        <DefaultInfoCard icon="flip_to_front" title="Impacto Ambiental" description="Nuestros campamentos están equipados con tecnología ecoamigable, incluyendo sistemas de energía renovable
+              y soluciones eficientes para la gestión de agua y residuos.
+              Al integrar estas tecnologías, FlexCamp no solo cumple con las necesidades operativas de nuestros
+              clientes, sino que también asegura un impacto ambiental mínimo." />
+      </div>
+      <div class="row justify-content-center">
+        <DefaultInfoCard icon="content_copy" title="Comodidad" description="Cada unidad de FlexCamp está diseñada para ofrecer confort y seguridad, garantizando que todos los
+              ocupantes tengan un espacio adecuado para su bienestar y eficiencia laboral.
+              La flexibilidad de nuestros servicios permite adaptar cada instalación a las especificaciones exactas de
+              cada proyecto, asegurando que todas las necesidades estén cubiertas desde el primer día." />
+        <DefaultInfoCard icon="flip_to_front" title="Rapidez" description="Con FlexCamp, las empresas pueden avanzar rápidamente en sus proyectos con la seguridad de que su
+              infraestructura temporal es no solo funcional y rápida de desplegar, sino también sostenible
+              y adaptada a las más estrictas normas de calidad y seguridad. EcoCamp es el aliado perfecto para proyectos
+              que demandan rapidez, eficiencia y responsabilidad ambiental." />
+      </div>
+    </div>
+  </div>
+
   <DefaultFooter />
 </template>
