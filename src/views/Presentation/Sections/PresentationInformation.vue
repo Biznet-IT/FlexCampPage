7f2b8c7fe3edf3760b3ea6f536bf1472021965a6
Sebastián Gómez Letelier <53100726+TheGreySam@users.noreply.github.com>
2024-05-08 11:59:26 -0400

    Update PresentationInformation.vue

--- src/views/Presentation/Sections/PresentationInformation.vue
+++ src/views/Presentation/Sections/PresentationInformation.vue
@@ -29,19 +29,19 @@ import DefaultInfoCard from "../../../examples/cards/infoCards/DefaultInfoCard.v
         <div class="col-lg-6 ms-auto">
           <div class="row justify-content-start">
             <div class="card m-3" style="width: 18rem;">
-              <img src="src\assets\img\containers\container-shop.png" class="card-img-top" alt="...">
+              <img src="/src/assets/img/containers/container-shop.png" class="card-img-top" alt="...">
               <div class="card-body">
                 <p class="card-text">Containers para Tiendas</p>
               </div>
             </div>
             <div class="card m-3" style="width: 18rem;">
-              <img src="src\assets\img\containers\container-office.png" class="card-img-top" alt="...">
+              <img src="/src/assets/img/containers/container-office.png" class="card-img-top" alt="...">
               <div class="card-body">
                 <p class="card-text">Containers para Tiendas</p>
               </div>
             </div>
             <div class="card m-3" style="width: 18rem;">
-              <img src="src\assets\img\containers\container-storage.png" class="card-img-top" alt="...">
+              <img src="/src/assets/img/containers/container-storage.png" class="card-img-top" alt="...">
               <div class="card-body">
                 <p class="card-text">Containers para Tiendas</p>
               </div>
